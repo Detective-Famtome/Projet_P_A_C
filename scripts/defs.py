bb32c26af2e0a4f73b9565955a5bf7cd4cf8d696
Detective-Famtome <detective.famtome@gmail.com>
2024-05-17 15:20:47 +0200

    Update defs.py

--- scripts/defs.py
+++ scripts/defs.py
@@ -1,9 +1,1 @@
-"""Module définition pour main.py"""
-import pygame
-from script import classes
 
-
-class Bouton:
-    def __init__(self, pos):
-    """Classe bouton pour les boutons"""
-    self.pos = pos
