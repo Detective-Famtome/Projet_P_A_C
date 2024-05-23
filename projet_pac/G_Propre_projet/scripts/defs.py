17732a20c6df87247cf027ffe51afd1e5ccad80e
Detective-Famtome <detective.famtome@gmail.com>
2024-05-23 20:10:52 +0200

    Update defs.py

--- projet_pac/G_Propre_projet/scripts/defs.py
+++ projet_pac/G_Propre_projet/scripts/defs.py
@@ -1,5 +1,5 @@
 def get_color():
-    """Donne les couleurs sous format de tuple (r, g, b)
+    """Donne les couleurs sous format de tuple (r, g, b)"""
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     GREEN = (0, 255, 0)
