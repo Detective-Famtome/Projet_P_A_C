f7471c426f66146d11e83fb42dd3ba3268b47dd5
Detective-Famtome <detective.famtome@gmail.com>
2024-05-25 19:58:32 +0200

    Update defs.py

--- projet_pac/G_Propre_projet/scripts/defs.py
+++ projet_pac/G_Propre_projet/scripts/defs.py
@@ -18,3 +18,30 @@ def setup_pygame():
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
     return screen, clock
+
+def imgs():
+    # Paths to images
+    bg_img_path = "projet_pac/G_Propre_projet/images/fonds/menu.jpg"
+    esc_img_path = "projet_pac/G_Propre_projet/images/boutons/keyboard_escape_outline.png"
+    to_quit_img_path = "projet_pac/G_Propre_projet/images/boutons/to_quit.png"
+    # Load images
+
+    if not os.path.exists(bg_img_path):
+        print(f"Error: The file '{bg_img_path}' does not exist.")
+        pygame.quit()
+        exit()
+
+    if not os.path.exists(esc_img_path):
+        print(f"Error: The file '{esc_img_path}' does not exist.")
+        pygame.quit()
+        exit()
+
+    if not os.path.exists(to_quit_img_path):
+        print(f"Error: The file '{to_quit_img_path}' does not exist.")
+        pygame.quit()
+        exit()
+
+    bg_img = pygame.image.load(bg_img_path)
+    esc_img = pygame.image.load(esc_img_path)
+    to_quit_img = pygame.image.load(to_quit_img_path)
+    return bg_img, esc_img, to_quit_img
